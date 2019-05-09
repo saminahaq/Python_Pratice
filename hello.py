57ba7d91bc1d6b165855c6c8339eb2506e038673
samina <saminanaseem@ymail.com>
2019-05-09 09:39:31 -0700

    added

---
+++ hello.py
@@ -0,0 +1,13 @@
+class Employee:
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.email = first + '.' + last + '@email.com'
+        self.pay = pay
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+emp_1 = Employee('Corey', 'Schafer', 50000)
+print(emp_1.fullname() +emp_1.fullname() )
